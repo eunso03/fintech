198fdf901bfac384d39af89adb525402d065645d
eunso03 <eunso1003@gmail.com>
2025-02-25 21:01:57 +0900

    database_01_DML

--- 01데이터베이스/DB_practice01_DML.sql
+++ 01데이터베이스/DB_practice01_DML.sql
@@ -150,5 +150,51 @@ WHERE Survived=1
 LIMIT 10;
 
 # 3.Passenger 테이블을 기준 ticket, survived테이블을 LEFT JOIN 한 결과에서 성별이 여성이면서 Pclass가 1인 사람 중 생존자(survived=1)를 찾아 이름, 성별, Pclass를 표시하시오.
+SELECT Name, Sex, Pclass FROM passenger p LEFT JOIN ticket t ON p.PassengerId = t.PassengerId 
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE (Sex="female" AND Pclass=1) AND survived=1;
 
+# 4. passenger, ticket, survived 테이블을 left join 후 나이가 10세 이상 20세 이하 이면서 Pclass 2인 사람 중 생존자를  표시하시오.
+SELECT * FROM passenger p LEFT JOIN ticket t ON p.PassengerId = t.PassengerId 
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE ((Age BETWEEN 10 AND 20) AND Pclass=2) AND survived=1;
+
+# 5. passenger, ticket, survived 테이블을 left join 후 성별이 여성 또는 Pclass 가 1인 사람 중 생존자를 표시하시오.
+SELECT * FROM passenger p LEFT JOIN ticket t ON p.PassengerId = t.PassengerId 
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE (Sex="female" OR Pclass=1) AND survived=1;
+
+# 6.  passenger, ticket, survived 테이블을 left join 후 생존자 중에서 이름에 Mrs가 포함된 사람을 찾아 이름, Pclass, 나이, Parch, Survived 를 표시하시오.
+SELECT Name, Pclass, Age, Parch, Survived FROM passenger p LEFT JOIN ticket t ON p.PassengerId = t.PassengerId 
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE survived=1 AND Name LIKE "%Mrs%";
+
+# 7. passenger, ticket, survived 테이블을 left join 후 Pclass가 1, 2이고 Embarked가 s, c 인 사람중에서 생존자를 찾아 이름, 성별, 나이를 표시하시오.
+SELECT Name, Sex, Age FROM passenger p LEFT JOIN ticket t ON p.PassengerId = t.PassengerId 
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE Pclass IN (1,2) AND Embarked IN ("S","C") AND survived=1;
+
+# 8. passenger, ticket, survived 테이블을 left join 후 이름에 James가 들어간 사람중 생존자를 찾아 이름, 성별, 나이 를 표시하고 나이를 기준으로 내림차순 정렬하시오.
+SELECT Name, Sex, Age FROM passenger p LEFT JOIN ticket t ON p.PassengerId = t.PassengerId 
+LEFT JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE survived=1 AND Name LIKE "%James%" 
+ORDER BY Age DESC;
+
+# 9. passenger, ticket, survived 테이블을 INNER JOIN한 데이터에서 성별별, 생존자의 숫자를 구하시오. 생존자 숫자 결과는 별칭을 Total로 하시오.
+SELECT Sex, count(survived) Total FROM passenger p INNER JOIN ticket t ON p.PassengerId = t.PassengerId 
+INNER JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE survived=1
+GROUP BY Sex;
+
+# 10. passenger, ticket, survived 테이블을 INNER JOIN한 데이터에서 성별별, 생존자의 숫자, 생존자 나이의 평균을 구하시오. 생존자 숫자 결과는 별칭을 Total로 하시오.
+SELECT Sex, count(survived) Total, AVG(Age) Total FROM passenger p INNER JOIN ticket t ON p.PassengerId = t.PassengerId 
+INNER JOIN survived s ON p.PassengerId = s.PassengerId
+WHERE survived=1
+GROUP BY Sex;
+
+# 11. passenger, ticket, survived 테이블을 INNER JOIN한 데이터에서 성별별, pclass별, 생존자별로 pclass, sex, survived , survived의 클래스별 합계, 생존자/사망자의 나이 평균을 구하시오. 
+# survived의 별칭은 is_survived로, 생존자 클래스별 합계는 별칭을 survived_total로, 생존자/사망자의 나이 평균은 별칭을 avg_age로 하시오.
+SELECT Pclass, sex, survived AS is_survived, count(survived) survived_total, AVG(Age) avg_age FROM passenger p INNER JOIN ticket t ON p.PassengerId = t.PassengerId 
+INNER JOIN survived s ON p.PassengerId = s.PassengerId
+GROUP BY Sex, Pclass, survived;
 
