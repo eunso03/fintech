3d135c0df1dcd913c0a13e351a3fd9c68e83e05c
eunso03 <eunso1003@gmail.com>
2025-04-09 12:57:32 +0900

    Selenium

--- 04data_scraping/book_dbio.py
+++ 04data_scraping/book_dbio.py
@@ -3,6 +3,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 import pandas as pd
 from datetime import datetime
+import time
 
 def dbconnect():
     engine = create_engine("mysql+pymysql://root:1234@localhost:3306/naver_book")
@@ -16,5 +17,6 @@ def to_book_db(keyword, df):
     """
     # DataBase 쿼리창 오픈
     conn = dbconnect()
+    time.sleep(1)
     df.to_sql(f"{keyword}_book_info", con=conn, if_exists="append", index=False)
     conn.close()
